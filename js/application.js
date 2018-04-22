52c04e08682c1a52436319fa7e460bfdc89054d9
Chris Sparks <chris@chrissparks.org.uk>
2018-04-22 17:54:19 +0100

    added blank template

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   /* initialise handlebarsjs templates
-  these reference <script> tags in the HTML file
+  these reference the ids of <script> tags in the HTML file
    */
   var templates = {
     note : Handlebars.compile(document.getElementById("template-note").innerHTML),
@@ -17,15 +17,15 @@ $(document).ready(function(){
     };
     $('#sidebar').append(templates.note(note));
     var noteMarker = $('<span class="note-marker" data-note-id="'+i+'"></span>').text('*').on('click',function(){
-      $('.sidebar-note,.note-marker').removeClass('bg-info');
-      $(this).addClass('bg-info');
-      $('.sidebar-note[data-note-id="'+i+'"').addClass('bg-info');
+      $('.sidebar-note,.note-marker').removeClass('bg-light border border-secondary text-danger');
+      $(this).addClass('text-danger');
+      $('.sidebar-note[data-note-id="'+i+'"').addClass('bg-light border border-secondary');
     });
     $(this).replaceWith(noteMarker);
   });
   $('#sidebar').on('click','.sidebar-note',function(){
-    $('.sidebar-note,.note-marker').removeClass('bg-info');
-    $(this).addClass('bg-info');
-    $('.note-marker[data-note-id="'+$(this).data('note-id')+'"').addClass('bg-info');
+    $('.sidebar-note,.note-marker').removeClass('bg-light border border-secondary text-danger');
+    $(this).addClass('bg-light border border-secondary');
+    $('.note-marker[data-note-id="'+$(this).data('note-id')+'"').addClass('text-danger');
   });
 });
